322e627d3dfa3ad9acdaf33f4e39aa3c6b1f2b16
unknown <voham1772016@gmail.com>
2020-12-02 22:53:42 +0700

    first commit

---
+++ do-an/js/js.js
@@ -0,0 +1,15 @@
+function openNav() {
+  document.getElementById("mySidenav").style.width = "350px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+
+$(document).ready(function(){
+  $('.owl-carousel').owlCarousel({
+   
+})
+});
+
+/** */
